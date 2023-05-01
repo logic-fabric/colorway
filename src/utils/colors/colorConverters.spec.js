16e4fcec7e663d2e7cf8f6dd4d6996fe681e7676
logic-fabric <lowic@logic-fabric.fr>
2023-05-01 11:33:15 +0200

    feature: add convertHexaToRGBCode() utility function

--- src/utils/colors/colorConverters.spec.js
+++ src/utils/colors/colorConverters.spec.js
@@ -1,19 +1,54 @@
-import { convertTo6DigitsHexaCode } from "./colorConverters";
+import {
+  convertHexaToRGBCode,
+  convertTo6DigitsHexaCode,
+} from "./colorConverters";
 
-const HEXA_3_DIGITS_TO_HAX_6_DIGITS = {
+const HEXA_3_DIGITS_TO_HEXA_6_DIGITS = {
   "#000": "#000000",
   "#fff": "#ffffff",
   "#fc9": "#ffcc99",
   "#0c3": "#00cc33",
 };
 
+const HEXA_3_DIGITS_TO_RGB = {
+  "#000": "rgb(0, 0, 0)",
+  "#fff": "rgb(255, 255, 255)",
+  "#fc9": "rgb(255, 204, 153)",
+  "#0c3": "rgb(0, 204, 51)",
+};
+
+const HEXA_6_DIGITS_TO_RGB = {
+  "#000000": "rgb(0, 0, 0)",
+  "#ffffff": "rgb(255, 255, 255)",
+  "#ffcc99": "rgb(255, 204, 153)",
+  "#00cc33": "rgb(0, 204, 51)",
+};
+
 describe("GIVEN a 3 digits hexadecimal color code", () => {
   describe("WHEN using convertTo6DigitsHexaCode()", () => {
-    test("THEN it can be converted in a 6 digits hexadecimal color code", () => {
-      for (let color in HEXA_3_DIGITS_TO_HAX_6_DIGITS) {
+    test("THEN it can be converted as a 6 digits hexadecimal color code", () => {
+      for (let color in HEXA_3_DIGITS_TO_HEXA_6_DIGITS) {
         expect(convertTo6DigitsHexaCode(color)).toBe(
-          HEXA_3_DIGITS_TO_HAX_6_DIGITS[color]
+          HEXA_3_DIGITS_TO_HEXA_6_DIGITS[color]
         );
+      }
+    });
+  });
+
+  describe("WHEN using convertHexaToRGBCode()", () => {
+    test("THEN it can be converted as a RGB color code", () => {
+      for (let color in HEXA_3_DIGITS_TO_RGB) {
+        expect(convertHexaToRGBCode(color)).toBe(HEXA_3_DIGITS_TO_RGB[color]);
+      }
+    });
+  });
+});
+
+describe("GIVEN a 6 digits hexadecimal color code", () => {
+  describe("WHEN using convertHexaToRGBCode()", () => {
+    test("THEN it can be converted as a RGB color code", () => {
+      for (let color in HEXA_6_DIGITS_TO_RGB) {
+        expect(convertHexaToRGBCode(color)).toBe(HEXA_6_DIGITS_TO_RGB[color]);
       }
     });
   });
--- src/utils/colors/colorConverters.ts
+++ src/utils/colors/colorConverters.ts
@@ -1,3 +1,19 @@
+export function convertHexaToRGBCode(colorCode: string): string {
+  if (colorCode.length === 4) {
+    colorCode = convertTo6DigitsHexaCode(colorCode);
+  }
+
+  const hexaRed = colorCode.slice(1, 3);
+  const hexaGreen = colorCode.slice(3, 5);
+  const hexaBlue = colorCode.slice(5);
+
+  const RGBRed = parseInt(hexaRed, 16);
+  const RGBGreen = parseInt(hexaGreen, 16);
+  const RGBBlue = parseInt(hexaBlue, 16);
+
+  return `rgb(${RGBRed}, ${RGBGreen}, ${RGBBlue})`;
+}
+
 export function convertTo6DigitsHexaCode(colorCode: string): string {
   const red = colorCode[1];
   const green = colorCode[2];
